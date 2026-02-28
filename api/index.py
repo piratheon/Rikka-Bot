75c5f2cbef96cdb5eb8bf6a9e29f9f00e8e02fad
piratheon <elibrahimichafiq2@gmail.com>
2026-02-28 19:52:36 +0000

    fixed some deps related bugs

--- api/index.py
+++ api/index.py
@@ -8,7 +8,14 @@ from telegram.ext import Application
 # Import our bot logic
 # We need to make sure src is in path for Vercel
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import os
+
+# We need to make sure src is in path for Vercel
+# Ensure absolute path calculation based on the file location
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
 
 from src.bot.app import build_application
 from src.config import Config
